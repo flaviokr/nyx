db3e3e05050a817008b575def73ab25829eb4ce4
Flavio Kon <flaviokr@gmail.com>
2015-06-19 17:11:26 +0000

    Create user (tecnico) model

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150610194515) do
+ActiveRecord::Schema.define(version: 20150619170901) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.string   "rf"
     t.string   "celular"
-    t.string   "telefone_resid"
     t.string   "comunicador"
-    t.string   "periodo_de_trab"
     t.boolean  "admin"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   add_index "users", ["rf"], name: "index_users_on_rf", unique: true
