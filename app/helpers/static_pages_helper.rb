db553e8809df1212fdf80b25ae265e4c8877cb5d
Flavio Kon <flaviokr@gmail.com>
2015-06-22 20:13:53 +0000

    Add a sidebar to the users show page

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,9 @@
 module StaticPagesHelper
+  
+  #Retorna true se o current_user for administrador
+  def admin?
+    if current_user
+      current_user.admin
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
   before_save { self.email = email.downcase }
-  before_save { self.rf = email.downcase }
+  before_save { self.rf = rf.downcase }
   
   validates :name,              presence: true, length: { maximum: 100 }
   validates :email,             presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
