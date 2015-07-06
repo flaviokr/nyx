0f19caf494dbb91b84b87024cf434c4c3bec49c7
Lucas Correa Silva <lucascorreasilvaa@gmail.com>
2015-07-06 16:03:00 +0000

    Atualizações no Layout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
 
-  root               'static_pages#home'
+  root               'sessions#new'
   get    'signup' => 'users#new'
-  get    'login'  => 'sessions#new'
   post   'login'  => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
   resources :users
--- db/migrate/20150701173744_drop_objetos_sectors.rb
+++
@@ -1,5 +0,0 @@
-class DropObjetosSectors < ActiveRecord::Migration
-  def change
-    drop_table :objetos_sectors
-  end
-end
