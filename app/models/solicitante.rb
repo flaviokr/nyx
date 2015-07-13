4b98a5774e25088acb70ddab649172b39b7cd68d
Vitor <vitorascorrea@gmail.com>
2015-07-13 12:23:32 -0300

    Unificacao Solicitante e Chamado

--- app/models/solicitante.rb
+++ app/models/solicitante.rb
@@ -9,7 +9,7 @@ class Solicitante < ActiveRecord::Base
   
   validates :rf,        presence: true, length: { maximum:  7  }, uniqueness: { case_sensitive: false }
   validates :nome,      presence: true, length: { maximum: 100 }
-  validates :email,     presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-  validates :ramal,     presence: true, length: { maximum: 200 }
+  validates :email,                     length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }, allow_nil: true
+  validates :ramal,                     length: { maximum: 200 }, allow_nil: true
   validates :sector_id, presence: true
 end
