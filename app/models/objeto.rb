c32d34f87f0055196940fc2377a3a1d5d4968ef9
Flavio Kon <flaviokr@gmail.com>
2015-07-21 14:24:04 +0000

    Validacao objeto

--- app/models/objeto.rb
+++ app/models/objeto.rb
@@ -3,5 +3,6 @@ class Objeto < ActiveRecord::Base
   has_many :objsecships
   has_many :sectors, through: :objsecships
   
-  validates :nome, presence: true, length: { maximum:  200  }, uniqueness: { case_sensitive: false }
+  validates :nome,      presence: true, length: { maximum:  200  }, uniqueness: { case_sensitive: false }
+  validates :categoria, presence: true, inclusion: { in: %w(H S R), message: "%{value} não está em um formato válido" }
 end
