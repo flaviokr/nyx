93f90f9dc5abcf27f6270715630dac8c7266f8c2
Vitor <vitorascorrea@gmail.com>
2015-09-16 19:03:33 -0300

    Alerta Funcional na pagina inicial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,19 +10,20 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @chamados = Chamado.all
-    @chamados_espera = Array.new
+    @chamados_estourados = Array.new
     @chamados_andamento = Array.new
     @chamados_concluido = Array.new
+
     @chamados.each do |chamado|
-      if chamado.status == "E"
-        @chamados_espera << chamado
-      elsif chamado.status == "A"
+      if chamado.status == "A" && Time.now.to_i - chamado.abertura.to_i > 100
+        @chamados_estourados << chamado
+      elsif chamado.status == "A" && !(Time.now.to_i - chamado.abertura.to_i > 100)
         @chamados_andamento << chamado
       elsif chamado.status == "C"
         @chamados_concluido << chamado
       end
     end
-    @chamados_espera.sort! { |a,b| b.prioridade <=> a.prioridade}   
+    @chamados_estourados.sort! { |a,b| b.prioridade <=> a.prioridade}   
     @chamados_andamento.sort! { |a,b| b.prioridade <=> a.prioridade} 
     @chamados_concluido.sort! { |a,b| b.prioridade <=> a.prioridade}   
   end
