69645c69fc76c4120933e4ff7830f28d2d0c0743
Flavio Kon <flaviokr@gmail.com>
2015-09-29 18:09:26 +0000

    Ugrade calculus filtro

--- app/assets/javascripts/calculos_filtro.js
+++ app/assets/javascripts/calculos_filtro.js
@@ -1,17 +1,39 @@
+$(function() {
+	$('#n_chamados').html($('#filtro td.abertura').length);
+	calcular();
+});
+
 $(document).change(function() {
+	calcular();
+});
+
+function calcular() {
 	var array_abertura = $('#filtro td.abertura');
 	var array_encerramento = $('#filtro td.encerramento');
 	var media = 0;
+	var contador = 0;
+	var enc = 0;
+	var abr = 0;
+	
 
-	for (i = 0; i < array_abertura.length; i++) {
-    	media = (media + moment(array_encerramento[i].innerHTML).unix() - moment(array_abertura[i].innerHTML).unix());
+	for (var i = 0; i < array_abertura.length; i++) {
+		enc = moment(array_encerramento[i].innerHTML).unix();
+		if (isNaN(enc)) {
+			contador++;
+		} else {
+			abr = moment(array_abertura[i].innerHTML).unix()
+	    media = media + enc - abr;
+		}
 	}
-
-	media = media/array_abertura.length;
+	
+	
+	media = media/(array_abertura.length - contador);
+	if (isNaN(media)) media = 0;
+	
 
 	media_horas = media/3600;
 	media_min = parseInt((media%3600)*60/3600);
 
 	$('#tempo_medio').html(parseInt(media_horas) + "&nbsp;horas&nbspe&nbsp;" + media_min + "&nbsp;minutos");
 	$('#n_chamados').html(array_abertura.length);
-});
+}
